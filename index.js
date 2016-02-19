33937c3181c40a98260d234b9c055bdcb045d737
周建 <mengzhidiguo@126.com>
2016-02-19 10:28:13 +0800

    4

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var app = express()
-
+app.use(express.static('./'));
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.sendFile('index.html');
 })
 
 app.listen(process.env.PORT || 5050)
