7773da075d25e2b62bfc53fe108a863a542c5336
周建 <mengzhidiguo@126.com>
2016-02-19 16:09:09 +0800

    new

--- app.js
+++ app.js
@@ -18,15 +18,12 @@ app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
 
 
 
 
 http.listen(require('./config.js').port,function(){
-    console.log("监听开始");
+    console.log("监听开始"+3000||require('./config.js').port);
 });
