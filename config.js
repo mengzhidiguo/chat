f25c461ade9951120533f9f0e29079a48a5fbc33
周建 <mengzhidiguo@126.com>
2016-03-01 21:55:39 +0800

    fff

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports  = {
     //数据库配置
-    mysql1:{
+    mysql0:{
         host     : 'localhost',
         user     : 'root',
         password : '',
@@ -12,9 +12,9 @@ module.exports  = {
         user     : 'SAE_MYSQL_USER',
         password : 'SAE_MYSQL_PASS',
         database : 'app_mengzhidiguo',
-        port:SAE_MYSQL_PORT||3306
+        port:'SAE_MYSQL_PORT'
     },
     //端口设置
     port:process.env.PORT || 5050,
-    port0:3000||process.env.PORT || 5050,
+    port1:3000||process.env.PORT || 5050,
 };
